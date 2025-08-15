c3552a69704c3503816c23192c80841db6625469
Kero2303 <kero2303@gmail.com>
2025-08-15 11:07:05 +0700

    First commit

---
+++ src/test/java/browsers/ChromeTest.java
@@ -0,0 +1,17 @@
+package browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ChromeTest {
+
+    @Test
+    void openBrowserWithDefaultMode(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+}
